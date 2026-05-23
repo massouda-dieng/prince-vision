805728015aa30156f521471bf61da6e3cc12c2b1
massouda <massoudadieng41@email.com>
2026-05-23 18:17:14 +0000

    Register Photo in admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Photo, Categorie
 
-# Register your models here.
+@admin.register(Categorie)
+class CategorieAdmin(admin.ModelAdmin):
+    list_display = ['nom']
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['titre', 'categorie', 'date_ajout']
+    list_filter = ['categorie']
